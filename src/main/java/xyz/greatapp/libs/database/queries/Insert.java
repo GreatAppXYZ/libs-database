b9790f4bdd1591203c0326ad3f3e7afe6c25e8f9
alayor <alayor3@gmail.com>
2017-08-22 20:52:05 -0500

    Moving Insert execution from database-service.

---
+++ src/main/java/xyz/greatapp/libs/database/queries/Insert.java
@@ -0,0 +1,66 @@
+package xyz.greatapp.libs.database.queries;
+
+import org.json.JSONObject;
+import xyz.greatapp.libs.database.adapter.DataBaseAdapter;
+import xyz.greatapp.libs.database.util.DbBuilder;
+import xyz.greatapp.libs.service.ServiceResult;
+import xyz.greatapp.libs.service.requests.database.ColumnValue;
+import xyz.greatapp.libs.service.requests.database.InsertQueryRQ;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Insert {
+
+    private final Common c = new Common();
+    private final DataBaseAdapter databaseAdapter;
+    private final String schema;
+    private final InsertQueryRQ query;
+
+    public Insert(DataBaseAdapter databaseAdapter, String schema, InsertQueryRQ query) {
+        this.databaseAdapter = databaseAdapter;
+        this.schema = schema;
+        this.query = query;
+    }
+
+    public ServiceResult execute() throws Exception {
+        String newId = databaseAdapter.executeInsert(new DbBuilder() {
+            @Override
+            public String sql() throws SQLException {
+                return "INSERT INTO "
+                        + schema + query.getTable() + " " +
+                        addValuesForInsert(query.getColumnValues(), query.getIdColumnName());
+            }
+
+            @Override
+            public ColumnValue[] values() {
+                return query.getColumnValues();
+            }
+
+            @Override
+            public JSONObject build(ResultSet resultSet) throws Exception {
+                return null;
+            }
+        });
+        return new ServiceResult(true, "", newId);
+    }
+
+    private String addValuesForInsert(ColumnValue[] columnValues, String idColumnName) {
+        StringBuilder valuesForInsert = new StringBuilder(" (");
+        String separator = "";
+        for (ColumnValue filter : columnValues) {
+            valuesForInsert.append(separator);
+            valuesForInsert.append(filter.getColumn());
+            separator = ", ";
+        }
+        valuesForInsert.append(") VALUES (");
+        separator = "";
+        for (ColumnValue ignored : columnValues) {
+            valuesForInsert.append(separator);
+            valuesForInsert.append("?");
+            separator = ", ";
+        }
+        valuesForInsert.append(") RETURNING ").append(idColumnName).append(";");
+        return valuesForInsert.toString();
+    }
+}
---
+++ src/test/java/xyz/greatapp/libs/database/queries/InsertTest.java
@@ -0,0 +1,51 @@
+package xyz.greatapp.libs.database.queries;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import xyz.greatapp.libs.database.adapter.DataBaseAdapter;
+import xyz.greatapp.libs.database.util.DbBuilder;
+import xyz.greatapp.libs.service.requests.database.ColumnValue;
+import xyz.greatapp.libs.service.requests.database.InsertQueryRQ;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InsertTest {
+
+    @Mock
+    private DataBaseAdapter databaseAdapter;
+    private Insert insert;
+
+    @Before
+    public void setUp() throws Exception {
+        when(databaseAdapter.executeInsert(any())).thenReturn("");
+    }
+
+    @Test
+    public void shouldConvertRequestOnSelectStatement() throws Exception {
+        // given
+        InsertQueryRQ query = new InsertQueryRQ("table", new ColumnValue[]{
+                new ColumnValue("column1", "value1"),
+                new ColumnValue("column2", "value2")
+        }, "id");
+
+        insert = new Insert(databaseAdapter, "greatappxyz.", query);
+
+        // whens
+        insert.execute();
+
+        // then
+        ArgumentCaptor<DbBuilder> dbBuilder = ArgumentCaptor.forClass(DbBuilder.class);
+        verify(databaseAdapter).executeInsert(dbBuilder.capture());
+
+        String sql = dbBuilder.getValue().sql();
+        assertEquals("INSERT INTO greatappxyz.table  (column1, column2) VALUES (?, ?) RETURNING id;", sql);
+    }
+}
